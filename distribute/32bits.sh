cf7e620484599fc18901d1d9fff30bb5d60cae9f
novice555 <novice_va_555@hotmail.com>
2013-11-07 14:15:20 +0700

    finish distribute func and split func mode_a

---
+++ distribute/32bits.sh
@@ -0,0 +1,1 @@
+gcc -m32 -Wall -ggdb -o distribute_32bits *.c split_file/*.c
--- distribute/copy_to_child.c
+++ distribute/copy_to_child.c
@@ -2,20 +2,18 @@
 #include<stdlib.h>
 #include"include/distribute.h"
 #include<stdio.h>
-static char cp_command[] = "cp -fR ";
 static char CHILD_PATH[] = "/koppae00/child/";
-static int copy(char *src, char *dest)
+static int xargs_copy(char *src_list, char *dst)
 {
     int sys;
     char strbuf[MAX_PATH] = "";
     if(system(NULL)==0)
         exit(EXIT_FAILURE);
-    strcat(strbuf, cp_command);
-    strcat(strbuf, src);
-    strcat(strbuf, " ");
-    strcat(strbuf, dest);
+    sprintf(strbuf, "xargs -a %s cp -frt %s", src_list, dst);
+    printf("%s\n", strbuf);
     sys = system(strbuf);
-    //printf("%d\n", sys);
+    if(sys == 0)
+        fprintf(stderr, "System Execute Error: Code %d.\n", sys);
     return 0;
 }
 
@@ -23,16 +21,24 @@ void copy_to_child(FileSource **Child, FileSourceProperties *ChildCount, int Max
 {
     char src[MAX_PATH];
     char dst[MAX_PATH];
+    FILE *tmpFile[MaxChild];
+    char tmpFileName[MaxChild][MAX_PATH];
     int i, j, n;
     for(i=0; i<MaxChild; i++)
     {
+        sprintf(tmpFileName[i], "tmp%d.txt", i+1);
+        tmpFile[i] = fopen(tmpFileName[i], "w");
         n = ChildCount[i]->count;
         for(j=0; j<n; j++)
         {
-            strcpy(src, Child[i][j]->path);
-            sprintf(dst, "%s0%d", CHILD_PATH, i+1);
-            copy(src, dst);
+            fprintf(tmpFile[i], "%s\n", Child[i][j]->path);
         }
+        fclose(tmpFile[i]);
+        sprintf(dst, "%s0%d", CHILD_PATH, i+1);
+        printf("%s\n", dst);
+        strcpy(src, tmpFileName[i]);
+        xargs_copy(src, dst);
+        remove(tmpFileName[i]);
     }   
 }
 
