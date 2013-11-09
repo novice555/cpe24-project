3dd6799e39f78f48f8d07705e2fff680da497c6d
novice555 <novice_va_555@hotmail.com>
2013-11-10 05:02:01 +0700

    test git add -u

--- distribute/split_file.c
+++
@@ -1,4 +0,0 @@
-void split_file(FileSource Parent, int *ParentCount, FileSource Child, int *ChildCount, void (*f)(FileSource, int*, FileSource, int*))
-{
-    (*f)(Parent, ParentCount, Child, ChildCount);    
-}
